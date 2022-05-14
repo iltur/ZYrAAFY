1b65884bd428c0dff49fb4e8ce2d9cc8784daa4d
iltur <chmielecka.joanna@gmail.com>
2022-05-14 15:15:18 +0200

    form

--- projects/contactus/src/lib/adapters/primary/components/contactus.component.ts
+++ projects/contactus/src/lib/adapters/primary/components/contactus.component.ts
@@ -1,24 +1,32 @@
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, ViewEncapsulation, ChangeDetectionStrategy, Inject } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ADDS_MESSAGES_DTO, AddsMessagesDtoPort } from '../../../application/ports/secondary/dto/adds-messages.dto-port';
 
 @Component({
-    selector: 'lib-contactus',
-    templateUrl: './contactus.component.html',
-    encapsulation: ViewEncapsulation.None,
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'lib-contactus',
+  templateUrl: './contactus.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContactusComponent {
-    readonly contact: FormGroup = new FormGroup({
-        name: new FormControl(),
-        email: new FormControl(),
-        text: new FormControl(),
-    });
+  readonly contact: FormGroup = new FormGroup({
+    name: new FormControl(),
+    email: new FormControl(),
+    text: new FormControl(),
+  });
 
   constructor(@Inject(ADDS_MESSAGES_DTO) private _addsMessagesDto: AddsMessagesDtoPort) {
   }
 
-  onMessageSubmited(): void {
-    this._addsMessagesDto.add();
+  onMessageSubmited(message: FormGroup): void {
+    this._addsMessagesDto.add({
+      name: message.get('name')?.value,
+      email: message.get('email')?.value,
+      text: message.get('text')?.value
+
+    }
+
+    );
   }
 }
