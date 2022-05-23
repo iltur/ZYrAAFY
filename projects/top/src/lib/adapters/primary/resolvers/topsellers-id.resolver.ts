afcdd2e0b33614c9f2d5043c726b437e7d57fa71
patip8999 <patip8999@gmail.comm>
2022-05-23 18:18:43 +0200

    topsellers działa

--- projects/top/src/lib/adapters/primary/resolvers/topsellers-id.resolver.ts
+++ projects/top/src/lib/adapters/primary/resolvers/topsellers-id.resolver.ts
@@ -1,10 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable, of } from 'rxjs';
-
+import { map } from 'rxjs';
+import { SetsStateTopsellersContextPort } from '../../../application/ports/secondary/context/sets-state-topsellers.context-port';
+import { SETS_STATE_TOPSELLERS_CONTEXT } from '../../../application/ports/secondary/context/sets-state-topsellers.context-port';
 @Injectable()
 export class TopsellersIdResolver implements Resolve<boolean> {
+  constructor(@Inject(SETS_STATE_TOPSELLERS_CONTEXT)private _setsStateTopsellersContext: SetsStateTopsellersContextPort){}
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return of(true);
+    return this._setsStateTopsellersContext.setState({ TopsellersId: route.params ['topsellersId']}).pipe(map(_ => true));
+   
   }
 }
