aad64ba82dd1367938a49c2478f6270931130d48
iltur <chmielecka.joanna@gmail.com>
2022-06-06 10:40:56 +0200

    auto-navigate to confirm from billing repaired

--- projects/billing/src/lib/adapters/primary/components/billing-detail.component.ts
+++ projects/billing/src/lib/adapters/primary/components/billing-detail.component.ts
@@ -40,7 +40,7 @@ export class BillingDetailComponent {
     private router: Router,
     @Inject(ADDS_ORDER_DETAIL_DTO)
     private _addsOrderDetailDto: AddsOrderDetailDtoPort
-  ) {}
+  ) { }
 
   onOrderDetailsSubmited(orderDetail: FormGroup): void {
     if (orderDetail.invalid) {
@@ -85,5 +85,6 @@ export class BillingDetailComponent {
     } else {
       this.emailValidation = false;
     }
+    this.router.navigate(['/confirmation'])
   }
 }
