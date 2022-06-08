d617b4a894d8a17822b2cd4d594dc6e8d2e7b304
patip8999 <patip8999@gmail.comm>
2022-06-08 11:41:46 +0200

    delete product of card

--- projects/product/src/lib/adapters/secondary/services/firebase-cart.service-module.ts
+++ projects/product/src/lib/adapters/secondary/services/firebase-cart.service-module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { FirebaseCartService } from './firebase-cart.service';
 import { GETS_ALL_CART_DTO } from '../../../application/ports/secondary/dto/gets-all-cart.dto-port';
+import { REMOVES_CART_DTO } from '../../../application/ports/secondary/dto/removes-cart.dto-port';
 
 @NgModule({
   imports: [AngularFirestoreModule],
@@ -9,6 +10,7 @@ import { GETS_ALL_CART_DTO } from '../../../application/ports/secondary/dto/gets
   providers: [
     FirebaseCartService,
     { provide: GETS_ALL_CART_DTO, useExisting: FirebaseCartService },
+    { provide: REMOVES_CART_DTO, useExisting: FirebaseCartService }
   ],
   exports: [],
 })
--- projects/product/src/lib/adapters/secondary/services/firebase-cart.service.ts
+++ projects/product/src/lib/adapters/secondary/services/firebase-cart.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { GetsAllCartDtoPort } from '../../../application/ports/secondary/dto/gets-all-cart.dto-port';
+import { RemovesCartDtoPort } from '../../../application/ports/secondary/dto/removes-cart.dto-port';
 import { CartDTO } from '../../../application/ports/secondary/dto/cart.dto';
 
 @Injectable()
-export class FirebaseCartService implements GetsAllCartDtoPort {
+export class FirebaseCartService implements GetsAllCartDtoPort, RemovesCartDtoPort {
   constructor(private _client: AngularFirestore) {}
 
   getAll(): Observable<CartDTO[]> {
     return this._client
       .collection<CartDTO>('carts')
       .valueChanges({ idField: 'id' });
+  }
+
+  remove(id: string): Observable<void> {
+    return from(this._client.doc('carts/'+id).delete()).pipe(map(() => void 0));
   }
 }
